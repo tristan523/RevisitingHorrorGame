2c08f792599d9f3119d9be54629b04b6c39cd33a
Tristan Hwang <Tristan Hwang@DESKTOP-0EINDUR.attlocal.net>
2021-04-21 17:26:43 -0700

    Initial commit

---
+++ desktop/src/com/tristan/games/revisitinghorror/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tristan.games.revisitinghorror.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tristan.games.revisitinghorror.RevisitingHorror;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RevisitingHorror(), config);
+	}
+}
